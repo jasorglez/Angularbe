d570f323d7a10733b5ebe828cbb1fee6feb8d7d9
Jose Angel Soriano <jsorglez@gmail.com>
2023-04-19 13:29:05 -0400

    Commit que contiene Login, Menus, token, mensajes, entornos general, validaciones, logos reales, despues me voy a la rama de Ambiente de Pruebas

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -8,10 +9,11 @@ import { Router } from '@angular/router';
 })
 export class SideBarComponent implements OnInit {
 
+ 
   constructor( private router: Router) { }
 
 	ngOnInit(): void  {
-    
+
    }
 
     /*=============================================
